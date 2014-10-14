4e27213babc408042c8cff09a98b1224d1ab3ed6
root <root@eb2-2238-pod20-t310.netlabs.ncsu.edu>
2014-10-14 18:34:55 -0400

    troubleshooting rvm.

--- manifests/compile.pp
+++ manifests/compile.pp
@@ -2,10 +2,12 @@ class passenger::compile {
 
   exec {'compile-passenger':
     path      => [ $passenger::gem_binary_path, '/usr/bin', '/bin', '/usr/local/bin' ],
-    command   => 'passenger-install-apache2-module -a',
+    command   => "${passenger::gem_binary_path}/passenger-install-apache2-module -a",
     logoutput => on_failure,
     creates   => $passenger::mod_passenger_location,
     timeout   => 0,
   }
+  Package <| tag == "passenger-dep" |> -> Exec['compile-passenger']
+  Package['passenger'] -> Exec['compile-passenger']
 
 }
